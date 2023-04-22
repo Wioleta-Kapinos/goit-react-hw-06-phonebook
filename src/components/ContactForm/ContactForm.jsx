b73ed396f6630a51752320668c86d0d14d7c235b
Wioleta-Kapinos <kapinos.wioleta19@gmail.com>
2023-04-22 20:14:53 +0200

    correct code

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -26,7 +26,7 @@ export const ContactForm = () => {
         dispatch(addContact(userBook));
         form.reset();
         }
-        
+
     return (
         <div className={css.contactForm}>
             <form onSubmit={handleSubmit} className={css.form}>
@@ -40,7 +40,6 @@ export const ContactForm = () => {
                     className={css.input}
                     name="name"
                     type="text"
-                    pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
                     title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
                     required
                 ></input>
@@ -53,7 +52,6 @@ export const ContactForm = () => {
                     id={numberInputId}
                     className={css.input}
                     type="tel"
-                    pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
                     name="number"
                     title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
                     required
